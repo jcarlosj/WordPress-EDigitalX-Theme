5cce3cff72e42d6f345356d5f750cc13945b149b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-04-28 19:09:31 -0500

    [+] front-page.php: Despliega 5 publicaciones destacadas (usando el Meta Box)

--- front-page.php
+++ front-page.php
@@ -25,6 +25,45 @@
             //  get_template_part( 'template-parts/entry-none' ); 
         ?> 
 
+        <?php 
+            
+            $args = array(
+                'posts_per_page' => 5,
+                'meta_key' => 'featured_post',
+                'meta_value' => 'yes'
+            );
+
+            $featured_post = new WP_Query( $args );
+
+            # echo '<pre>';   var_dump( $featured_post );     echo '</pre>';   
+        ?>
+            <div class="lista-destacados">
+                <h2>Publicaciones Destacadas</h2>
+                
+                    <?php 
+                        if ( $featured_post -> have_posts() ) : 
+                            while( $featured_post -> have_posts() ) : 
+                                $featured_post -> the_post(); 
+
+                                ?>
+                                    
+                                    <h3>
+                                        <a href="<?php the_permalink(); ?>"> 
+                                            <?php the_title(); ?>
+                                        </a>
+                                    </h3>
+                                    <p class="detalles">De <a href="<?php the_author_posts() ?>"><?php the_author(); ?> </a> / <?php echo get_the_date('F j, Y'); ?> / In <?php the_category(', '); ?></p>
+                    
+                                <?php
+                    
+                            endwhile;
+                        else:
+                        endif;
+                        wp_reset_postdata(); 
+                    ?>
+                
+            </div>
+
     </div>
 
 <?php 
