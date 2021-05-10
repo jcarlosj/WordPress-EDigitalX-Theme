5149d2d5149c68fbd5f2263ebfcef2865899985a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-05-10 15:03:00 -0500

    [+] front-page.php: Ultima entrada

--- template-parts/posts-featured/last-post.php
+++ template-parts/posts-featured/last-post.php
@@ -1,52 +1,88 @@
-<?php 
-    $args = array(
-        'numberposts' => 1,         // Number of recent posts thumbnails to display
-        'post_status' => 'publish'  // Show only the published posts
+<?php
+    $args = array (
+        'post_type' => 'post',
+        'posts_per_page' => 1,
+        'orderby' => 'post_date',
+        'order' => 'DESC',
     );
 
-    $recent_posts = wp_get_recent_posts( $args );
+    $popular_posts = new WP_Query( $args );
 
-    // echo '<pre>';   var_dump( $recent_posts );     echo '</pre>'; 
-    // echo '<pre>';   var_dump( get_the_category( $recent_posts[ 0 ][ 'ID' ] ) );     echo '</pre>'; 
-    // echo '<pre>';   var_dump( get_post_meta( $recent_posts[ 0 ][ 'ID' ], 'estimated_reading_time', true ) );     echo '</pre>'; 
+    // echo '<pre>';   var_dump( $popular_posts );     echo '</pre>';   
 
-    $estimated_time = get_post_meta( $recent_posts[ 0 ][ 'ID' ], 'estimated_reading_time', true );      #   Obtengo el valor del Meta Box
-    $has_estimated_time = ( $estimated_time == '' || $estimated_time == 0 ) ? false : true;
+    if ( $popular_posts -> have_posts() ) : 
+        while( $popular_posts -> have_posts() ) : 
+            $popular_posts -> the_post(); 
 
+            $estimated_time = get_post_meta( get_the_ID(), 'estimated_reading_time', true );        #   Obtengo el valor del Meta Box
+            $has_estimated_time = ( $estimated_time == '' || $estimated_time == 0 ) ? false : true;
+
+            ?>
+
+                <article class="entry">
+
+                    <div class="entry__thumbnail entry__thumbnail--position">
+                        <a class="entry__thumbnail-link" href="<?php the_permalink(); ?>">
+                            <?php 
+                                the_post_thumbnail( 
+                                    'entry-landscape', 
+                                    [
+                                        'class' => 'entry__featured-image entry__featured-image--rounded'
+                                    ]
+                                ); ?>
+                        </a>
+                    </div>
+
+                    <div class="entry__content entry__content--size entry__content--position">
+
+                        <header class="entry__header">
+                                            
+                            <span class="entry__categories">
+                                <?php the_category( ' ' ); ?>
+                            </span>
+                            
+                            <h2 class="entry__title">
+                                <a class="entry__title-link" href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+
+                        </header>
+
+                        <p class="entry__excerpt">
+                            <?php if( has_excerpt() ): ?>
+                                <?php 
+                                    $excerpt = wp_strip_all_tags( get_the_excerpt() );
+                                    echo $excerpt; 
+                                ?>
+                            <?php else: ?>
+                                <?php echo wp_trim_words( get_the_content(), 18, '..' ); ?>
+                            <?php endif; ?>
+                        </p>
+
+                        <footer class="entry__footer">
+                            <time class="entry__time" datetime="<?php the_time( 'Y-m-d' ); ?>">
+                
+                                <?php 
+                                    if( $has_estimated_time ) :
+                                        ?>
+                                            <span class="entry__time-read"><?php echo $estimated_time; ?> <?php esc_html_e( 'min read', 'edigitalx' ); ?></span>
+                                        <?php
+                                    endif;
+                                ?>
+                                <span class="entry__date"><?php echo get_the_time( 'M' ); ?> <?php echo get_the_time( 'd' ); ?>, <?php echo get_the_time( 'Y' ); ?></span>
+                        
+                            </time>
+                        </footer>
+
+                    </div>
+
+                </article><!--  -->
+
+            <?php
+
+        endwhile;
+    else:
+    endif;
+    wp_reset_postdata(); 
 ?>
-
-<article id="post-<?php echo $recent_posts[ 0 ][ 'ID' ]; ?>" class="last-post">
-
-    <div class="post-media">
-        <a href="<?php echo get_permalink( $recent_posts[ 0 ][ 'ID' ] ); ?>" class="thumbnail-link">    
-            <?php echo get_the_post_thumbnail( $recent_posts[ 0 ][ 'ID' ], 'posts-featured-landscape-mobile' ); ?>
-        </a>
-    </div>
-    
-    <div class="post-content">
-        
-        <div class="post-catetory">
-            <a href="#" rel="category tag">
-                <?php echo get_the_category( $recent_posts[ 0 ][ 'ID' ] ) [ 0 ] -> name; ?>
-            </a>
-        </div>
-        <a href="<?php echo get_permalink( $recent_posts[ 0 ][ 'ID' ] ); ?>">
-            <h2 class="post-title"><?php echo $recent_posts[ 0 ][ 'post_title' ]; ?></h2>
-        </a>
-        
-    </div>
-
-    <time class="post-footer" datetime="<?php the_time( 'Y-m-d' ); ?>">
-        
-        <?php 
-            if( $has_estimated_time ) :
-                ?>
-                    <span class="post-read-time"><?php echo $estimated_time; ?> <?php esc_html_e( 'min read', 'edigitalx' ); ?></span>
-                <?php
-            endif;
-        ?>
-        <span class="post-date"><?php echo get_the_time( 'M' ); ?> <?php echo get_the_time( 'd' ); ?>, <?php echo get_the_time( 'Y' ); ?></span>
-
-    </time>
-
-</article><!-- #post-## -->
