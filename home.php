31f1889f2bc749d6305b6ebb18d7d8a86b8b3934
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-05-14 17:33:11 -0500

    [+] home.php: Cantidad publicaciones & paginas (Hero)

--- home.php
+++ home.php
@@ -15,25 +15,86 @@
 
     <div id="site-container" class="home">
 
+        <?php 
+            /** Obtenemos el numero de paginas */
+            $paged = ( get_query_var( 'paged' ) ) 
+                ?   get_query_var( 'paged' ) 
+                :   1;
+            # echo 'page: ' .$paged;
+
+            /** */
+            $total_post_count = wp_count_posts();
+            $published_post_count = $total_post_count -> publish;
+            $total_pages = ceil( $published_post_count / $posts_per_page );
+        ?>
+
         <header 
-                class="hero"
-                <?php if( $featured_image_id) : ?>
-                    style="background-image: url( <?php echo $image_source; ?> );"
-                <?php else: ?>
-                    
-                <?php endif; ?>
+            class="hero"
+            <?php if( $featured_image_id ) : ?>
+                style="background-image: url( <?php echo $image_source; ?> );"
+            <?php else: ?>
+                
+            <?php endif; ?>
         >
-            <div class="hero-content container">
-                <h1><?php echo get_the_title( $blog_page_id ); ?></h1>
+            <div class="container">
+
+                <h1 class="hero__title"><?php echo get_the_title( $blog_page_id ); ?></h1>
+                <ul class="hero__data">
+
+                    <li class="hero__item hero__item--publish">
+                        <span class="hero__number hero__number--publish">
+                            <?php echo $published_post_count; ?>
+                        </span>
+                        <span class="hero__description hero__description--publish">
+                            <?php esc_html_e( 'Publish', 'edigitalx' ); ?>
+                        </span>
+                    </li>
+
+                    <li class="hero__item hero__item--pages">
+                        <span class="hero__number hero__number--pages">
+                            <?php echo $total_pages; ?>
+                        </span>
+                        <span class="hero__description hero__description--pages">
+                            <?php esc_html_e( 'Pages', 'edigitalx' ); ?>
+                        </span>
+                    </li>
+                    
+                </ul>
             </div>
+            
         </header>
 
-        <section class="section container with-sidebar">
+        <section class="section with-sidebar">
             <main class="main-content">
 
-                <?php get_template_part( 'template-parts/entries-featured/entry', 'last' ); ?>
+                <?php
+                    /** Verifica que solo se vea la ultima entrada en la primera pagina */
+                    if( $paged <= 1 ) : 
+                        
+                        ?>
+                        
+                        <section class="posts-featured">
+                            <div class="container">
+                                <div class="featured-content">
+                                    
+                                    <?php get_template_part( 'template-parts/entries-featured/entry', 'last' ); ?>
 
-                <section class="entries-blog">
+                                    <div class="posts-featured-sidebar">
+
+                                        hello!
+                                        
+                                    </div><!-- .posts-featured-sidebar  -->
+
+                                </div>
+                            </div>
+                        </section>
+
+                        <?php
+
+                    endif;
+                ?>
+
+                <section class="container entries-blog">
 
                     <?php 
 
