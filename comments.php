a396ed72012c8f582f1b5f497c68b4554059413e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-05-25 11:21:32 -0500

    [+] comments.php: Agrega formulario de comentarios para las entradas

--- comments.php
+++ comments.php
@@ -5,3 +5,43 @@
 
     do_action( 'get_file_name', basename( __FILE__ ) );
 ?>
+
+<section class="container comments">
+    <?php 
+        $args = [
+            'class_submit' => 'btn btn-primary'
+        ];
+        comment_form( $args ); 
+    ?>
+
+    <h3 class="comment-title">
+        <?php _e( 'Comments', 'pizzashop' ); ?>
+    </h3>
+    <ul class="comment-list">
+        <?php  
+            $args = [
+                'post_id'   => $post -> ID,
+                'status'    => 'approve'
+            ];
+            $comments = get_comments( $args );
+
+            #   Valida si no esta vacio el array de comentarios
+            if( ! empty( $comments ) ) {
+                $args = [
+                    'per_page'  => 10,
+                    'reverse_top_level' => false
+                ];
+                wp_list_comments( $args, $comments );
+            }
+            else {
+                ?>
+                    <p class="comment-message">
+                        <?php _e( 'There are no comments', 'pizzashop' ); ?>
+                    </p>
+                <?php
+            }
+
+        ?>
+    </ul>
+
+</section>
