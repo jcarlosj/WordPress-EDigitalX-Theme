69dc6d2813edf2c8d3d41bff6b50fb7db69ce8f4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-07-29 19:35:07 -0500

    [$] template-tags.php: Obtener metabox de tiempo estimado de lectura y modificar todos los archivos que requieren esta funcionalidad

--- search.php
+++ search.php
@@ -108,7 +108,7 @@
 
 									<div class="<?php echo $args[ 'is_entry_featured' ] ? 'entry-featured entry-featured__content--size entry-featured__content--position' : 'entry__content entry__content--size entry__content--position'; ?>">
 
-										<?php get_template_part( 'template-parts/entry', 'header' ); ?>
+										<?php get_template_part( 'template-parts/entry', 'header', $args ); ?>
 										<?php get_template_part( 'template-parts/entry', 'content' ); ?>
 										<?php get_template_part( 'template-parts/entry', 'footer', $args ); ?>
 
