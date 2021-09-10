5dfb2f5e745760f556cb1a1620f3563c6a5214e9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-09-10 12:09:25 -0500

    [+] footer.php: icono telegram

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
 
     do_action( 'get_file_name', basename( __FILE__ ) );
 ?>
+    <a class="telegram" target="_blank" href="https://t.me/explicaciondigital"></a>
     <a class="smooth-scroll" href="#top">
         <span class="dashicons dashicons-arrow-up-alt"></span>
     </a>
