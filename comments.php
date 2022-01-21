c5fe47a8dc89241cd475d2b9d76cb2c2a75f84a3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-01-21 10:37:36 -0500

    [+] single.php: Estilos campo comentarios cuando el usuario esta logueado

--- comments.php
+++ comments.php
@@ -7,41 +7,50 @@
 ?>
 
     <section class="comments container">
-        <?php
-            $args = [
-                'class_submit' => 'btn btn-primary',
-            ];
-            comment_form( $args );
-        ?>
 
-        <h3 class="comment-title">
-            <?php _e( 'Comments', 'edigitalx' ); ?>
-        </h3>
-        <ul class="comment-list">
+        <?php 
+            $class_logged_in = ( is_user_logged_in() ) ? 'form-logged-in' : '';
+        ?>
+        
+        <div class="<?php echo $class_logged_in; ?>">
+
             <?php
                 $args = [
-                    'post_id'   => $post -> ID,
-                    'status'    => 'approve'
+                    'class_submit' => 'btn btn-primary',
                 ];
-                $comments = get_comments( $args );
-
-                #   Valida si no esta vacio el array de comentarios
-                if( ! empty( $comments ) ) {
-                    $args = [
-                        'per_page'  => 10,
-                        'reverse_top_level' => false
-                    ];
-                    wp_list_comments( $args, $comments );
-                }
-                else {
-                    ?>
-                        <p class="comment-message">
-                            <?php _e( 'There are no comments', 'edigitalx' ); ?>
-                        </p>
-                    <?php
-                }
-
+                comment_form( $args );
             ?>
-        </ul>
+
+            <h3 class="comment-title">
+                <?php _e( 'Comments', 'edigitalx' ); ?>
+            </h3>
+            <ul class="comment-list">
+                <?php
+                    $args = [
+                        'post_id'   => $post -> ID,
+                        'status'    => 'approve'
+                    ];
+                    $comments = get_comments( $args );
+
+                    #   Valida si no esta vacio el array de comentarios
+                    if( ! empty( $comments ) ) {
+                        $args = [
+                            'per_page'  => 10,
+                            'reverse_top_level' => false
+                        ];
+                        wp_list_comments( $args, $comments );
+                    }
+                    else {
+                        ?>
+                            <p class="comment-message">
+                                <?php _e( 'There are no comments', 'edigitalx' ); ?>
+                            </p>
+                        <?php
+                    }
+
+                ?>
+            </ul>
+
+        </div>
 
     </section>
