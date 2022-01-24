8bbdf2068241fc59754033afd1c9ae11fb192d5b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-01-24 11:22:29 -0500

    [+] Dashicon: Tiempo de lectura estimada de la entrada

--- template-parts/related-posts.php
+++ template-parts/related-posts.php
@@ -140,17 +140,18 @@
                                         <footer class="entry__footer">
                                             <time class="entry__time" datetime="<?php the_time( 'Y-m-d' ); ?>">
 
+                                            <span class="<?php echo $args[ 'is_entry_featured' ] ? 'entry-featured__date' : 'entry__date'; ?>"><?php echo get_the_time( 'M' ); ?> <?php echo get_the_time( 'd' ); ?>, <?php echo get_the_time( 'Y' ); ?></span>
                                                 <?php
                                                     if( $args[ 'has_estimated_time' ] ) :
                                                         ?>
                                                             <span class="<?php echo $args[ 'is_entry_featured' ] ? 'entry-featured__time-read' : 'entry__time-read'; ?>">
+                                                            <span class="dashicons dashicons-clock"></span>
                                                                 <?php echo $args[ 'estimated_time' ]; ?>
                                                                 <?php esc_html_e( 'min read', 'edigitalx' ); ?>
                                                             </span>
                                                         <?php
                                                     endif;
                                                 ?>
-                                                <span class="<?php echo $args[ 'is_entry_featured' ] ? 'entry-featured__date' : 'entry__date'; ?>"><?php echo get_the_time( 'M' ); ?> <?php echo get_the_time( 'd' ); ?>, <?php echo get_the_time( 'Y' ); ?></span>
 
                                             </time>
                                         </footer>
