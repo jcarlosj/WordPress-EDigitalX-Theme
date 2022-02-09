2db9b79db88c6b5d5d7c97172a5e018dff76c962
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-02-09 11:58:25 -0500

    [+] header.php: Ajuste estilos (Mobile/Desktop) respecto al menu admin de WordPress (modo usuario logueado)

--- header.php
+++ header.php
@@ -15,50 +15,48 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-
+    <div id="top">&nbsp;</div>
     <?php wp_body_open(); ?>
 
-    <div id="theme-container">
-    
-    <header id="header" class="container-fluid">
+    <div id="theme-container" class="<?php echo ( is_user_logged_in() ) ? 'show-wp-admin-menu' : ''; ?>">
+        <header id="header" class="container-fluid<?php echo ( is_user_logged_in() ) ? ' show-wp-admin-menu' : ''; ?>">
 
-        <div class="site-branding">
-            <div class="navbar-toggler">
-                <a href="#top" id="menu-icon" class="menu menu-icon">
-                    <div class="menu-icon__bar-1"></div>
-                    <div class="menu-icon__bar-2"></div>
-                    <div class="menu-icon__bar-3"></div>
-                </a>
+            <div class="site-branding">
+                <div class="navbar-toggler">
+                    <a href="#top" id="menu-icon" class="menu menu-icon">
+                        <div class="menu-icon__bar-1"></div>
+                        <div class="menu-icon__bar-2"></div>
+                        <div class="menu-icon__bar-3"></div>
+                    </a>
+                </div>
+
+                <?php wpex_logo(); ?>
+
+            </div>
+            <div class="site-navigation">
+
+                <?php
+                    wpex_logo();
+                    if( has_nav_menu( 'edigitalx-header-menu' ) ):
+
+                        $args = [
+                            'theme_location'    => 'edigitalx-header-menu',
+                            'container'         => 'nav',
+                            'container_id'      => 'menu',
+                            'container_class'   => 'menu menu-header not-display'
+                        ];
+                        wp_nav_menu( $args );
+                ?>
+
+                <?php endif; ?>
+
+            </div>
+            <div class="site-search">
+                <a href="#top" id="search-icon" class="search-icon dashicons dashicons-search"></a>
+                <!-- <span id="search-icon__close" class="dashicons dashicons-no-alt search-icon"></span> -->
             </div>
 
-            <?php wpex_logo(); ?>
-
-        </div>
-        <div class="site-navigation">
-
-            <?php
-                wpex_logo();
-                if( has_nav_menu( 'edigitalx-header-menu' ) ):
-
-                    $args = [
-                        'theme_location'    => 'edigitalx-header-menu',
-                        'container'         => 'nav',
-                        'container_id'      => 'menu',
-                        'container_class'   => 'menu menu-header not-display'
-                    ];
-                    wp_nav_menu( $args );
-            ?>
-
-            <?php endif; ?>
-
-        </div>
-        <div class="site-search">
-            <a href="#top" id="search-icon" class="search-icon dashicons dashicons-search"></a>
-            <!-- <span id="search-icon__close" class="dashicons dashicons-no-alt search-icon"></span> -->
-        </div>
-
-    </header>
-    <?php get_search_form(); ?>
-    <div id="top">&nbsp;</div>
-    <div id="background">&nbsp;</div>
+        </header>
+        <?php get_search_form(); ?>
+        <div id="background">&nbsp;</div>
 
